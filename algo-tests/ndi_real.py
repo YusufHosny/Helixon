102e89f7eac6fb2fba7fbb096d2821f1eb9f530c
Yusuf Hosny <yusufyy2004@hotmail.com>
2024-11-10 03:09:58 +0100

    prepared some ndi code for real data

--- algo-tests/ndi_real.py
+++ algo-tests/ndi_real.py
@@ -7,38 +7,37 @@ from scipy.spatial.transform import Rotation
 # get data from hdf5
 raw_timestamp, raw_9dof, raw_rpy, raw_bno, raw_bmp, raw_pressure, gt_timestamp, gt_position, gt_orientation = readHDF5('up_and_down')
 
+# convenience
+X, Y, Z = 0, 1, 2
+N = len(raw_timestamp)
+
 # get sensor data
-accel = np.array(raw_9dof[:, :3])
+araw = np.array(raw_9dof[:, :3])
 gyro = np.array(raw_9dof[:, 3:6])
 magn = np.array(raw_9dof[:, 6:])
+ts = raw_timestamp
 
-roll = raw_rpy[:, 0]
-pitch = raw_rpy[:, 1]
-yaw = raw_rpy[:, 2]
-
+# rotate acceleration to global coords
+accel = np.zeros_like(araw)
+for i, rpyi in enumerate(raw_rpy):
+    rot = Rotation.from_euler('xyz', rpyi, degrees=True).inv()
+    accel[i] = rot.apply(araw[i])
 
 # remove gravity vector
-for i, (ai, ri, pi, yi) in enumerate(zip(accel, roll, pitch, yaw)):
-    rot = Rotation.from_euler('xyz', [ri, pi, yi], degrees=True)
-    ai -= rot.apply([0, 0, 9.81])
+accel[:, Z] += 9.81
 
-
-# dead reckoning ndi on real data
-pos = np.zeros((120, 3)) + np.array(gt_position[0, :]).reshape(1, 3)
+# dead reckoning ndi on synth
+pos = np.zeros((N, 3))
 vi = np.array([0., 0., 0.])
-tprev = 0
-for i, (ti, psi, ai, gi, mi, ri, pi, yi) in enumerate(zip(raw_timestamp, pos, accel, gyro, magn, roll, pitch, yaw)):
-    rot = Rotation.from_euler('xyz', [-ri, -pi, -yi], degrees=True)
-    vi +=  rot.apply(ai)*(ti-tprev)*1e-6
-    psi += [vij*(ti-tprev)*1e-6 for vij in vi]
-    tprev = ti
+for i in range(1, N):
+    dt = (ts[i]-ts[i-1])
+    vi += accel[i]*dt
+    pos[i] = pos[i-1] + vi*dt + 0.5*accel[i]*dt**2
+    
 
-# plot ground truth
+# plot synth ndi and gt
 fig = plt.figure()
 ax = plt.axes(projection='3d')
-ax.plot3D(gt_position[:120, 0], gt_position[:120, 1], gt_position[:120, 2], 'gray')
-
-# plot ndi
 ax.plot3D(pos[:, 0], pos[:, 1], pos[:, 2], 'blue')
-
+ax.plot3D(gt_position[:, 0], gt_position[:, 1], gt_position[:, 2], 'gray')
 plt.show()
