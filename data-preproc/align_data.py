c60da78005e371b1993a851e9a6f994b3c67a197
Yusuf Hosny <yusufyy2004@hotmail.com>
2025-04-21 21:18:44 +0200

    updated data preproc to synchronize with cross correl

--- data-preproc/align_data.py
+++ data-preproc/align_data.py
@@ -9,7 +9,7 @@ import os
 import numpy as np
 from scipy.spatial.transform import Rotation
 from model.spiral_model import Spiral
-
+from scipy import signal
 
 # creating spiral model
 spiral_pitch = 4.2 #m
@@ -28,6 +28,70 @@ def quaternion_multiply(q1, q2):
     y = w1 * y2 - x1 * z2 + y1 * w2 + z1 * x2
     z = w1 * z2 + x1 * y2 - y1 * x2 + z1 * w2
     return np.array([w, x, y, z])
+
+def angular_distance(b, a):
+    return np.where(np.abs(b-a) < 180, b-a, np.where((b-a) > 0, 360 - (b-a), 360 + (b-a)))
+
+def determine_delay(raw_rpy, raw_timestamp, gt_orientation, gt_timestamp):
+    ts = np.array(raw_timestamp)
+
+    rots = np.array([Rotation.from_quat(q).as_euler('xyz', degrees=True) for q in gt_orientation]) # change quat gt rots to euler
+    rots -= rots[0] # normalize to initial rot
+    rots = rots[:, ::-1] # reverse order of xyz to match ndof
+    rots[:, X] *= -1 # invert X rotations to match ndof
+    rots = np.array([Rotation.from_euler('xyz', r, degrees=True).as_euler('xyz', degrees=True) for r in rots])
+
+    # lerp rots
+    N = len(rots)
+
+    # interpolate over timestamps
+    lerped_rotations = np.zeros((N, 3))
+    for i in range(N):
+        if gt_timestamp[i] in ts:
+            lerped_rotations[i] = raw_rpy[np.argmax(ts == gt_timestamp[i])]
+        else:
+            # lerp
+            ix_2 = np.argmax(ts > gt_timestamp[i])
+            ix_1 = ts.shape[0] - np.argmax(np.flip(ts, axis=0) < gt_timestamp[i]) - 1
+
+            lerped_rotations[i] = raw_rpy[ix_1] + (gt_timestamp[i]-ts[ix_1])*angular_distance(raw_rpy[ix_2], raw_rpy[ix_1])/(ts[ix_2] - ts[ix_1] + 1e-10)
+
+    rpy = lerped_rotations
+
+    # interpolate over time to create single sampling rate
+    fs = len(gt_timestamp) / gt_timestamp[-1]
+    CNT = int(fs*gt_timestamp[-1])
+    unified_ts = np.linspace(0, gt_timestamp[-1], CNT)
+
+    lerpedrpy = np.zeros((CNT, 3))
+    lerpedrots = np.zeros((CNT, 3))
+
+    for i in range(CNT):
+        if unified_ts[i] in gt_timestamp:
+            lerpedrpy[i] = rpy[np.argmax(gt_timestamp == unified_ts[i])]
+            lerpedrots[i] = rots[np.argmax(gt_timestamp == unified_ts[i])]
+
+        else:
+            # lerp
+            ix_2 = np.argmax(gt_timestamp > unified_ts[i])
+            ix_1 = gt_timestamp.shape[0] - np.argmax(np.flip(gt_timestamp, axis=0) < unified_ts[i]) - 1
+
+            lerpedrpy[i] = rpy[ix_1] + (unified_ts[i]-gt_timestamp[ix_1])*angular_distance(rpy[ix_2], rpy[ix_1])/(gt_timestamp[ix_2] - gt_timestamp[ix_1] + 1e-10)
+            lerpedrots[i] = rots[ix_1] + (unified_ts[i]-gt_timestamp[ix_1])*angular_distance(rots[ix_2], rots[ix_1])/(gt_timestamp[ix_2] - gt_timestamp[ix_1] + 1e-10)
+
+    rots = lerpedrots
+    rpy = lerpedrpy
+
+
+    fs = len(unified_ts) / unified_ts[-1]
+    n = len(rpy[:, X])
+
+    corr = signal.correlate(rots[:, X], rpy[:, X], mode='same') / np.sqrt(signal.correlate(rpy[:, X], rpy[:, X], mode='same')[int(n/2)] * signal.correlate(rots[:, X], rots[:, X], mode='same')[int(n/2)])
+
+    delay_arr = np.linspace(-0.5*n, 0.5*n, n)
+    delay = delay_arr[np.argmax(corr)]
+
+    return delay / fs
 
 
 data_raw = readAll()
@@ -69,11 +133,20 @@ for name, di, posi in zip(dataset_names, data_raw, pos):
     p0 = raw_pressure.max()
     raw_pressure[np.where(raw_pressure < 100000)] = p0
 
+    # apply cross correlation
+    delay = determine_delay(raw_rpy, raw_timestamp, gt_orientation, gt_timestamp)
+    gt_timestamp = np.array(gt_timestamp)
+    gt_orientation = np.array(gt_orientation)
+    gt_timestamp -= delay
+    gt_orientation = gt_orientation[np.where(gt_timestamp >= 0)]
+    gt_position = gt_position[np.where(gt_timestamp >= 0)]
+    gt_timestamp = gt_timestamp[np.where(gt_timestamp >= 0)]
+
     raw_data = np.array([np.stack((t, ndof[0], ndof[1], ndof[2], ndof[3], ndof[4], ndof[5], ndof[6], ndof[7], ndof[8], rpy[0], rpy[1], rpy[2], bno, bmp, pres)) for (t, ndof, rpy, bno, bmp, pres) in zip(raw_timestamp, raw_9dof, raw_rpy, raw_bno, raw_bmp, raw_pressure)])
     gt_data = np.array([np.stack((t, pi[0], pi[1], pi[2], rpyi[0], rpyi[1], rpyi[2], rpyi[3])) for (t, pi, rpyi) in zip(gt_timestamp, posi, gt_orientation)])
     storeAsHDF5_path(file_path, raw_data, gt_data, wifidata)
 
-PLOT = True
+PLOT = False
 if PLOT:
     # plot all datasets
     X, Y, Z = 0, 1, 2
